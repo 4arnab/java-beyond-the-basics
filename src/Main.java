a2f825208b0cc439456ba3a4f998801edf8c852a
4arnab <axmedarnab3@gmail.com>
2025-11-11 06:00:21 +0530

    program args

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args){
+        if(args.length > 0){
+            System.out.println(Arrays.toString(args));
+        }
+
         char grade = 'A';
 
         String result = switch (grade){
